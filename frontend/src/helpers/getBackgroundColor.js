04132a59fd284abc25a84000c0340c79ef0a0b6a
kz3ko <xiixikx@gmail.com>
2022-01-06 17:26:50 +0100

    Added warm and hot colors to background when there is no people.

--- frontend/src/helpers/getBackgroundColor.js
+++ frontend/src/helpers/getBackgroundColor.js
@@ -8,14 +8,24 @@ export function getBackgroundColor(
     numberOfPeoples
 ) {
     const temp = temperature.toFixed(2);
-    if (Math.floor(temp) === coldDownTemp && numberOfPeoples === 0) {
-        return 'rgba(0, 255, 0, 0.3)';
+    if (numberOfPeoples === 0) {
+        const warmHotDiff = warmThreshold[1] - warmThreshold[0]
+        if (temp < coldDownTemp) {
+            return 'rgba(0, 100, 255, 0.2)';
+        } else if (temp > coldDownTemp && temp <= coldDownTemp + warmHotDiff) {
+            return 'rgba(255, 255, 0, 0.3)';
+        } else if (temp > coldDownTemp + warmHotDiff) {
+            return 'rgba(255, 0, 0, 0.3)';
+        } else {
+            return 'rgba(0, 255, 0, 0.3)';
+        }
     }
+
     if (temp >= coldThreshold[0] && temp < coldThreshold[1]) {
         return 'rgba(0, 100, 255, 0.2)';
-    } else if (temp >= optimalThreshold[0] && temp < optimalThreshold[1]) {
+    } else if (temp >= optimalThreshold[0] && temp <= optimalThreshold[1]) {
         return 'rgba(0, 255, 0, 0.3)';
-    } else if (temp >= warmThreshold[0] && temp < warmThreshold[1]) {
+    } else if (temp > warmThreshold[0] && temp < warmThreshold[1]) {
         return 'rgba(255, 255, 0, 0.3)';
     } else if (temp >= hotThreshold[0]) {
         return 'rgba(255, 0, 0, 0.3)';

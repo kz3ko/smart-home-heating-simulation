34efe595f7ee2f032113b1085ce6ae54ee0dddf6
kz3ko <xiixikx@gmail.com>
2022-05-28 14:59:53 +0200

    Removed endpoints, move routes to route53.tf

--- infrastructure/scripts/user-data.sh
+++ infrastructure/scripts/user-data.sh
@@ -18,19 +18,19 @@ Content-Transfer-Encoding: 7bit
 Content-Disposition: attachment; filename="userdata.txt"
 
 #!/bin/bash
-#sudo yum update
-#sudo yum search docker
-#sudo yum info docker
-#sudo yum install docker
-#
-#USER="ssm-user"
-#sudo usermod -aG docker $USER
-#
-#suwget https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)
-#sudo mv docker-compose-$(uname -s)-$(uname -m) /usr/local/bin/docker-compose
-#sudo chmod -v +x /usr/local/bin/docker-compose
-#
-#sudo systemctl enable docker.service
-#sudo systemctl start docker.service
 
-echo "Hello world" >> usr/bin/tmp/testfile.txt
+# Install docker
+sudo yum update
+sudo yum install docker
+
+USER="ssm-user"
+sudo usermod -aG docker $USER
+
+# Install docker-compose
+sudo wget https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)
+sudo mv docker-compose-$(uname -s)-$(uname -m) /usr/local/bin/docker-compose
+sudo chmod -v +x /usr/local/bin/docker-compose
+
+# Enable and start docker service
+sudo systemctl enable docker.service
+sudo systemctl start docker.service
--- infrastructure/vpc.tf
+++ infrastructure/vpc.tf
@@ -4,86 +4,8 @@ resource "aws_vpc" "main" {
   enable_dns_support   = true
 }
 
-resource "aws_internet_gateway" "main" {
-  vpc_id = aws_vpc.main.id
-}
-
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
-}
-
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
-}
-
-resource "aws_route" "public_internet_gateway" {
-  route_table_id         = aws_route_table.public.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.main.id
-}
-
-resource "aws_route_table_association" "public" {
-  subnet_id      = aws_subnet.public.id
-  route_table_id = aws_route_table.public.id
-}
-
-resource "aws_vpc_endpoint" "ssm" {
-  vpc_id              = aws_vpc.main.id
-  service_name        = "com.amazonaws.${data.aws_region.current.name}.ssm"
-  vpc_endpoint_type   = "Interface"
-  private_dns_enabled = true
-
-  security_group_ids = [
-    aws_security_group.default.id
-  ]
-
-  subnet_ids = [
-    aws_subnet.public.id
-  ]
-}
-
-resource "aws_vpc_endpoint" "ssmmessages" {
-  vpc_id              = aws_vpc.main.id
-  service_name        = "com.amazonaws.${data.aws_region.current.name}.ssmmessages"
-  vpc_endpoint_type   = "Interface"
-  private_dns_enabled = true
-
-  security_group_ids = [
-    aws_security_group.default.id
-  ]
-
-  subnet_ids = [
-    aws_subnet.public.id
-  ]
-}
-
-resource "aws_vpc_endpoint" "ec2messages" {
-  vpc_id              = aws_vpc.main.id
-  service_name        = "com.amazonaws.${data.aws_region.current.name}.ec2messages"
-  vpc_endpoint_type   = "Interface"
-  private_dns_enabled = true
-
-  security_group_ids = [
-    aws_security_group.default.id
-  ]
-
-  subnet_ids = [
-    aws_subnet.public.id
-  ]
-}
-
-resource "aws_vpc_endpoint" "s3" {
-  vpc_id              = aws_vpc.main.id
-  service_name        = "com.amazonaws.${data.aws_region.current.name}.s3"
-  vpc_endpoint_type   = "Interface"
-
-  security_group_ids = [
-    aws_security_group.default.id
-  ]
-
-  subnet_ids = [
-    aws_subnet.public.id
-  ]
 }
